bcfe36d6e637deb36b8507e406cc10295dec4024
Juan Lagunas <dev.jlagunas@gmail.com>
2024-05-14 07:31:42 +0900

    change img url path for neptune.

--- src/views/NeptuneView.vue
+++ src/views/NeptuneView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import PlanetDisplay from "@/components/PlanetDisplay.vue";
-import uranus from "@/assets/planets/planet-uranus.svg";
-import uranusInternal from "@/assets/planets/planet-uranus-internal.svg";
+import neptune from "@/assets/planets/planet-neptune.svg";
+import neptuneInternal from "@/assets/planets/planet-neptune-internal.svg";
 
 const tabs = [
   {
@@ -11,7 +11,7 @@ const tabs = [
     content:
       "Neptune is the eighth and farthest-known Solar planet from the Sun. In the Solar System, it is the fourth-largest planet by diameter, the third-most-massive planet, and the densest giant planet. It is 17 times the mass of Earth, more massive than its near-twin Uranus.",
     source: "https://en.wikipedia.org/wiki/Neptune",
-    img: uranus,
+    img: neptune,
   },
   {
     id: "structure",
@@ -20,7 +20,7 @@ const tabs = [
     content:
       "Neptune's internal structure resembles that of Uranus. Its atmosphere forms about 5% to 10% of its mass and extends perhaps 10% to 20% of the way towards the core. Increasing concentrations of methane, ammonia and water are found in the lower regions.",
     source: "https://en.wikipedia.org/wiki/Neptune#Internal_structure",
-    img: uranusInternal,
+    img: neptuneInternal,
   },
   {
     id: "geology",
@@ -29,7 +29,7 @@ const tabs = [
     content:
       "Neptune's atmosphere is 80% hydrogen and 19% helium. A trace amount of methane is also present. Prominent absorption bands of methane exist at wavelengths above 600 nm, in the red and infrared portion of the spectrum.",
     source: "https://en.wikipedia.org/wiki/Neptune#Atmosphere",
-    img: uranus,
+    img: neptune,
   },
 ];
 
